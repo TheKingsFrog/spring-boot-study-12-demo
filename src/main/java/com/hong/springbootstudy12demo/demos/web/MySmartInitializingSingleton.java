af8db60d4c487b5499750a457c51eec95f4481ca
洪成隆 <chenglong.hong@hand-china.com>
2024-02-26 16:50:38 +0800

    [add]spring-applicationContext学习-6

---
+++ src/main/java/com/hong/springbootstudy12demo/demos/web/MySmartInitializingSingleton.java
@@ -0,0 +1,12 @@
+package com.hong.springbootstudy12demo.demos.web;
+
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MySmartInitializingSingleton implements SmartInitializingSingleton {
+    @Override
+    public void afterSingletonsInstantiated() {
+        System.out.println("所有的bean已经被实例化了");
+    }
+}
