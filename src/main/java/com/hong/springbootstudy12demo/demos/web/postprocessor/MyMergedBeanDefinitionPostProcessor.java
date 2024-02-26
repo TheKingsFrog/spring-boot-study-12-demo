860999ce986c6b8d0772d202231cb3215a04cd07
洪成隆 <chenglong.hong@hand-china.com>
2024-02-26 23:55:27 +0800

    [add]spring-applicationContext学习-7

---
+++ src/main/java/com/hong/springbootstudy12demo/demos/web/postprocessor/MyMergedBeanDefinitionPostProcessor.java
@@ -0,0 +1,11 @@
+package com.hong.springbootstudy12demo.demos.web.postprocessor;
+
+import org.springframework.beans.factory.support.MergedBeanDefinitionPostProcessor;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+
+public class MyMergedBeanDefinitionPostProcessor implements MergedBeanDefinitionPostProcessor {
+    @Override
+    public void postProcessMergedBeanDefinition(RootBeanDefinition beanDefinition, Class<?> beanType, String beanName) {
+
+    }
+}
